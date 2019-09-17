aa2cd2701333375e54288b42331bd9a264fbd3c3
Carter Jones <36671987+cartermjones@users.noreply.github.com>
2019-09-17 14:48:32 -0500

    Update FileReader.java

--- task1-src/FileReader.java
+++ task1-src/FileReader.java
@@ -1,4 +1,50 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FileReader {
-
+	
+	String parsedLine;
+	int categoryCount = 1;
+	String category;
+	
+	public void readFiles(File[] files, FileWriter tsvWriter) throws IOException {
+		
+		FileParser gumbo = new FileParser();
+		
+		for (File file : files) 
+		{
+			if(file.isDirectory()) {
+				category = file.getName();
+				System.out.printf("%d of 20 directories analyzed \n", categoryCount);
+				categoryCount++;
+				readFiles(file.listFiles(), tsvWriter);
+			} else {
+				gumbo.parseFile(file, category);
+				parsedLine = gumbo.returnParsedLine();
+				generateFile(tsvWriter, parsedLine);
+			}
+		}
+	}
+	
+	public void generateFile(FileWriter tsvWriter, String parsedLine) {
+		try {
+			tsvWriter.write(parsedLine);
+			tsvWriter.write("\n");
+		} catch(IOException ex) {
+			System.out.printf("ERROR: %s\n", ex);
+		}
+	}
+	
+	public void flushAndCloseWriter(FileWriter tsvWriter) {
+		try {
+			tsvWriter.flush();
+			tsvWriter.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
 }
