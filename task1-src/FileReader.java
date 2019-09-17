cea6430cb624df9a11db610a1aabb23a5f87195c
Carter Jones <36671987+cartermjones@users.noreply.github.com>
2019-09-17 15:03:30 -0500

    Update FileReader.java

--- task1-src/FileReader.java
+++ task1-src/FileReader.java
@@ -12,8 +12,13 @@ public class FileReader {
 		
 		FileParser gumbo = new FileParser();
 		
+		//This is lightly modified boilerplate code for traversing a file system.
 		for (File file : files) 
 		{
+			//If the file is a directory, we take the name of the directory and designate it the category for the upcoming messages.
+			//We also alert the user to the progress through the file system.
+			//If the file is not a directory, our FileParser (gumbo) will parse the file, 
+			//move the data to a parsedLine variable, and add it to the .tsv file under construction.
 			if(file.isDirectory()) {
 				category = file.getName();
 				System.out.printf("%d of 20 directories analyzed \n", categoryCount);
@@ -27,6 +32,7 @@ public class FileReader {
 		}
 	}
 	
+	//This method is used to write to the .tsv file under construction.
 	public void generateFile(FileWriter tsvWriter, String parsedLine) {
 		try {
 			tsvWriter.write(parsedLine);
@@ -36,12 +42,12 @@ public class FileReader {
 		}
 	}
 	
+	//This method flushes and closes the FileWriter - everything packaged neatly into one method for ease of use.
 	public void flushAndCloseWriter(FileWriter tsvWriter) {
 		try {
 			tsvWriter.flush();
 			tsvWriter.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
