ab136992151978d29a4f80d4db1a886cd519d1ef
Carter Jones <36671987+cartermjones@users.noreply.github.com>
2019-09-17 14:47:23 -0500

    Update FileGenerator.java

--- task1-src/FileGenerator.java
+++ task1-src/FileGenerator.java
@@ -1,4 +1,17 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FileGenerator {
-
+	
+	public static void main(String[] args) throws IOException {
+		File[] files = new File("C:\\Users\\Carter Jones\\Desktop\\Big Data\\20-newsgroups\\").listFiles();
+		FileWriter tsvWriter = new FileWriter("task1-src.tsv");
+		
+		FileReader julep = new FileReader();
+		
+		julep.readFiles(files, tsvWriter);	
+		julep.flushAndCloseWriter(tsvWriter);
+		System.out.println("Your .tsv file is ready for business! Thank you for your patience.");
+	}
 }
